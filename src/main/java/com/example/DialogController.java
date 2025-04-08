9aaa29bc504999318e769de9c0de60deafd66904
Erős István <ers@zold.lan>
2025-04-08 08:52:30 +0200

    Kezdés

---
+++ src/main/java/com/example/DialogController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DialogController {
+
+    @FXML
+    void onClickCloseButton(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    void onClickShowButton(ActionEvent event) {
+        showDialog();
+    }
+
+    void showDialog() {
+        try {
+            tryShowDialog();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    void tryShowDialog() throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("dialogScene.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
