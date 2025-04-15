05ea8ef8aaae4390376c5b137148cff6e9ac5713
Your Name <you@example.com>
2025-04-15 11:07:50 +0200

    Törlés gomb ok

--- src/main/java/com/example/DialogController.java
+++ src/main/java/com/example/DialogController.java
@@ -2,8 +2,11 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class DialogController {
 
@@ -14,7 +17,9 @@ public class DialogController {
 
     @FXML
     void onClickCloseButton(ActionEvent event) {
-
+        startClose();        
+    }
+    private void startClose() {
         // Stage stage = 
         // (Stage) 
         // ((javafx.scene.Node) event.getSource())
@@ -22,12 +27,29 @@ public class DialogController {
         // .getWindow();
 
         // stage.close();
+        if(plateField.getText().isEmpty()) {
+            System.out.println("üres");
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("Hiba");
+            alert.setTitle("Üres");
+            alert.setContentText("A rendszám nem lehet üres!");
+            alert.showAndWait();
+            return;
+        }
         mainController.carList.getItems().add(plateField.getText());
         mainController.stage.close();
-        
     }
+
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+    }
+    
+    @FXML
+    void onKeyReleasedPlateField(KeyEvent event) {
+        if(event.getCode() == KeyCode.ENTER) {
+            startClose();
+        }
+        
     }
 
 }
--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,7 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class MainController {
@@ -58,5 +60,35 @@ public class MainController {
         controller.setMainController(this);
 
     }
+
+    @FXML
+    void onClickDelButton(ActionEvent event) {
+        System.out.println("törlés...");
+        int index = this.carList
+            .getSelectionModel()
+            .getSelectedIndex();
+        if(index == -1) {
+            System.err.println("Hiba! Ki kell jelölni valamit!");
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Hiba");
+            alert.setHeaderText("Kiválasztás!");
+            alert.setContentText("Hiba! Nincs kiválaszva rendszám!");
+            alert.showAndWait();
+            return;
+        }
+        String res = this.carList.getItems().remove(index);
+        System.out.println("Törölve: " + res);
+    }
+
+    @FXML
+    void onClickExitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickModifyButton(ActionEvent event) {
+
+    }
+    
 
 }
